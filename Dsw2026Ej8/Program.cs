2baeda55ba91cf4c2810df600add4d535f2b8baf
MiltonTolaba <Milton.Tolaba@alu.frt.utn.edu.ar>
2026-05-04 13:52:00 -0300

    Resolucion Problema 4

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -16,6 +16,12 @@
             Problema3 q = new Problema3();
             string resultado1 = q.CompararCopias(5, prod);
             Console.WriteLine(resultado1);
+
+            Problema4 c = new Problema4();
+            Console.WriteLine("Caso 1: " + c.CalcularPromedio(6, 8, 10));
+            Console.WriteLine("Caso 2: " + c.CalcularPromedio(8, null, 3));
+            Console.WriteLine("Caso 3: " + c.CalcularPromedio(null, null, null));
+
         }
     }
 }
