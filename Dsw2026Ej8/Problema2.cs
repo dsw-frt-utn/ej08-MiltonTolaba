572c89e4cb2c3b1a7d409e94703e7d220cf187d8
MiltonTolaba <Milton.Tolaba@alu.frt.utn.edu.ar>
2026-05-04 00:45:56 -0300

    Resolucion Problema 2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total = quantity > 0 ? quantity * unitPrice: 0;
+
+            var venta = new
+            {
+                code = productCode,
+                description = productDescription,
+                quantity = quantity,
+                total = total
+            };
+
+            return $"{venta.code}-{venta.description}-{venta.total}";
+        }
+    }
+}
--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -7,6 +7,10 @@
             ProductHelper helper = new ProductHelper();
             string etiqueta = helper.ObtenerEtiquetaProducto(101, "Teclado Mecanico", 75000);
             Console.WriteLine(etiqueta);
+
+            Problema2 p = new Problema2();
+            string resultado = p.CrearResumenVenta(102, "Mouse", 4, 75000);
+            Console.WriteLine(resultado);
         }
     }
 }
